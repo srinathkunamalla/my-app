f6d24433a76c84fb474b1b87844004ffc7e8d281
Srinath Kunamalla <s0k022y@homeoffice.wal-mart.com>
2020-05-26 00:20:04 -0500

    Updates

---
+++ precache-manifest.309f1c50dc44eae3f724d9c0398c33b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228b98d6d70111a8197ac1482ceb8260",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7a48b32c9000e6f63266",
+    "url": "/my-app/static/css/main.c51f3011.chunk.css"
+  },
+  {
+    "revision": "6715995dac0646eebbc1",
+    "url": "/my-app/static/js/2.1b33c47c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.1b33c47c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a48b32c9000e6f63266",
+    "url": "/my-app/static/js/main.753eb592.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
